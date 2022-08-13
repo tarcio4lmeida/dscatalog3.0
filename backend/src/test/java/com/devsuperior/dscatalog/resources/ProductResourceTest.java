b47f131e3d561012b79d27771d9c1222d0a48d7b
tarcio almeida <tarcioalmeidalima@gmail.com>
2022-08-13 18:47:57 -0300

    02-30 Começando testes na camada web

---
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTest.java
@@ -0,0 +1,53 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.factory.Factory;
+import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dto.ProductDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductResource.class) // carrega o contexto porém somente da camanda web
+public class ProductResourceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean //quando carrega o contexto
+    private ProductService service;
+
+    private PageImpl<ProductDTO> page;
+
+    @InjectMocks
+    private ProductResource resource;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        ProductDTO productDTO = Factory.createProductDTO();
+        page = new PageImpl<>(List.of(productDTO));
+
+        when(service.findAllPaged((Pageable) any())).thenReturn(page);
+
+    }
+
+    @Test
+    public void findAllShouldReturnPageOfProductDTO() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products")).andExpect(status().isOk());
+    }
+}
