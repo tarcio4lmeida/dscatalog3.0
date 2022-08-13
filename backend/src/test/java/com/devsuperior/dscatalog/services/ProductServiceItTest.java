f1cd8f7bfae9969e21c2467a328ed320f954bab2
tarcio almeida <tarcioalmeidalima@gmail.com>
2022-08-13 20:22:27 -0300

    02-37 Teste de integração para findAllPaged

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceItTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceItTest.java
@@ -4,7 +4,6 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.factory.Factory;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
-import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourcesNotFoundException;
 import com.devsuperior.dto.ProductDTO;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,14 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest //carrrega o contexto
+@Transactional // da rollback em cada test
 public class ProductServiceItTest {
     @Autowired
     private ProductService service;
@@ -67,14 +70,35 @@ public class ProductServiceItTest {
     }
 
     @Test
-    public void findAllPagedShouldReturnPage() {
-        Page<ProductDTO> result = service.findAllPaged((Pageable) any());
+    public void findAllPagedShouldReturnPage0Size10() {
+        PageRequest pageRequest = PageRequest.of(0,10);
+        Page<ProductDTO> result = service.findAllPaged(pageRequest);
 
+        assertFalse(result.isEmpty());
         assertNotNull(result);
-
-        verify(repository, times(1)).findAll((Pageable) any());
+        assertEquals(0, result.getNumber());
+        assertEquals(10, result.getSize());
+        assertEquals(countTotalProducts, result.getTotalElements());
     }
 
+    @Test
+    public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+        PageRequest pageRequest = PageRequest.of(50,10);
+        Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+        PageRequest pageRequest = PageRequest.of(0,10, Sort.by("name"));
+        Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+        assertFalse(result.isEmpty());
+        assertEquals("Macbook Pro", result.getContent().get(0).getName());
+        assertEquals("PC Gamer", result.getContent().get(1).getName());
+        assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+    }
     @Test
     public void findByIdShouldReturnAProductDTOWhenIdExists() {
         ProductDTO productDTO = service.findbyId(existingId);
